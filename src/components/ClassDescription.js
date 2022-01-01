b31984fae0cd0058007fcfd04abae258484714f0
richardles <richardles.2312@gmail.com>
2022-01-01 23:49:28 +0700

    student view log enhanced, header remake, log loading state fixed, notification prototype and some little fix

--- src/components/ClassDescription.js
+++ src/components/ClassDescription.js
@@ -324,7 +324,7 @@ const ClassDescription = () => {
                             <div className="w-0 flex-1 flex items-center">
                                 <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
                                 <span className="ml-2 flex-1 w-0 truncate" ref={addToAssignmentBoxRefs}>
-                                    -
+                                    Empty
                                 </span>
                             </div>
                             <div className="ml-4 flex-shrink-0 flex space-x-4">
@@ -365,7 +365,7 @@ const ClassDescription = () => {
                             <div className="w-0 flex-1 flex items-center">
                                 <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
                                 <span className="ml-2 flex-1 w-0 truncate" ref={addToFinalExamBoxRefs}>
-                                    -
+                                    Empty
                                 </span>
                             </div>
                             <div className="ml-4 flex-shrink-0 flex space-x-4">
